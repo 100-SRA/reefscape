c1113d0e442cdd2c0905e2bb97df04b6616723a4
Caleb Brown <dizziboi07@gmail.com>
2025-02-05 18:28:32 -0500

    Added drivetrain motors to Constraints

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,4 +29,18 @@ public final class Constants {
     /*How far the robot will drive out the starting zone for autonomous*/
     public static final double kAutoLeaveDistance = 1.5;
   }
+public static class DriverConstraints {
+  /* The side with the battery is the front */
+  /*Spark Numbers  
+   * 1 - Back left BL
+   * 2 - Front Left FL
+   * 3 - Front Right FR
+   * 4 - Back Left BR
+   */
+  public static final int kPortPWM_Drivetrain_BL = 0;
+  public static final int kPortPWM_Drivetrain_Fl = 1;
+  public static final int kPortPWM_Drivetrain_FR = 4;
+  public static final int kPortPWM_Drivetrain_Br = 5;
+}
+
 }
