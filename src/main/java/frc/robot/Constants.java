67c2d53206420cc91f156871ec2b07ca1c69700e
Malik Saafir <msaafir99@gmail.com>
2025-02-08 16:16:45 -0800

    formatting

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,16 +5,20 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
-    //The port number for the controller
+    // The port number for the controller
     public static final int kDriverControllerPort_A = 0;
     public static final int kDriverControllerPort_B = 1;
     /* Binding */
@@ -23,22 +27,23 @@ public final class Constants {
     public static final int kX_Button = 3;
     public static final int kCircle_Button = 4;
     public static final int kTriangle_Button = 5;
-    public static final int kSquare_button = 6; 
+    public static final int kSquare_button = 6;
   }
-  
+
   public static class AutoConstants {
-    /*How far the robot will drive out the starting zone for autonomous*/
+    /* How far the robot will drive out the starting zone for autonomous */
     public static final double kAutoLeaveDistance = 1.5;
   }
 
   public static class DriveConstants {
     /* The side with the battery is the front */
-    /*Spark Numbers  
-    * 1 - Back Left BL
-    * 2 - Front Left FL
-    * 3 - Front Right FR
-    * 4 - Back Right BR
-    */
+    /*
+     * Spark Numbers
+     * 1 - Back Left BL
+     * 2 - Front Left FL
+     * 3 - Front Right FR
+     * 4 - Back Right BR
+     */
     public static final int kPortPWM_Drivetrain_BL = 0;
     public static final int kPortPWM_Drivetrain_FL = 1;
     public static final int kPortPWM_Drivetrain_FR = 4;
@@ -48,14 +53,13 @@ public final class Constants {
   public static class ArmConstants {
     /* Can id For the main arm lift */
     public static final int kCanid_ArmLift = 1;
-    public static final double kArmSpeed = 0.5; //sets the speed for the arm
+    public static final double kArmSpeed = 0.5; // sets the speed for the arm
   }
 
   public static class AlgaeArmConstants {
     /* Can id for the algae intake mechanism */
     public static final int kCanid_AlgaeIntakeArm = 2;
-    public static final double kAlgaeArmSpeed = 0.4; //sets the speed of the arm
+    public static final double kAlgaeArmSpeed = 0.4; // sets the speed of the arm
   }
 
-  
 }
--- src/main/java/frc/robot/subsystems/LiftArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftArmSubsystem.java
@@ -4,33 +4,29 @@
 
 package frc.robot.subsystems;
 
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
 import frc.robot.Constants.ArmConstants;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import java.util.function.DoubleSupplier;
 
-
 public class LiftArmSubsystem extends SubsystemBase {
-  /** Main arm motor are using a NEO motor connected with CAN  */
-  private final SparkMax m_armLiftMotor = new SparkMax(ArmConstants.kCanid_ArmLift,MotorType.kBrushless );
-  
- public LiftArmSubsystem() {
-   setDefaultCommand(
-   runOnce(() -> m_armLiftMotor.disable()));
- }
+  /** Main arm motor are using a NEO motor connected with CAN */
+  private final SparkMax m_armLiftMotor = new SparkMax(ArmConstants.kCanid_ArmLift, MotorType.kBrushless);
 
-  public void MoveArm(boolean on){
-     // main command for moving the arm
-     if(on){
-      m_armLiftMotor.set(ArmConstants.kArmSpeed); // modify speed based on max output constant
-     } else{
-      m_armLiftMotor.set(ArmConstants.kArmSpeed * -1);
-     }
-     
-    
+  public LiftArmSubsystem() {
+    setDefaultCommand(
+        runOnce(() -> m_armLiftMotor.disable()));
   }
 
+  public void MoveArm(boolean on) {
+    // main command for moving the arm
+    if (on) {
+      m_armLiftMotor.set(ArmConstants.kArmSpeed); // modify speed based on max output constant
+    } else {
+      m_armLiftMotor.set(ArmConstants.kArmSpeed * -1);
+    }
+
+  }
 
 }
