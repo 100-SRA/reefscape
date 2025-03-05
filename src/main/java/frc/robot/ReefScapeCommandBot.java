cf2cf42421694232c23e7780e9a60b228f165625
Caleb Brown <dizziboi07@gmail.com>
2025-03-05 08:26:33 -0500

    Updated controls for the robot

--- src/main/java/frc/robot/ReefScapeCommandBot.java
+++ src/main/java/frc/robot/ReefScapeCommandBot.java
@@ -36,8 +36,8 @@ public class ReefScapeCommandBot {
     m_driverControllerB.L1().whileTrue(m_liftArm.moveArmCommand(true)); //lift arm will go up when left stick is pressed 
     m_driverControllerB.R1().whileTrue(m_liftArm.moveArmCommand(false)); // lift arm will go down when right stick is pressed 
 
-    m_driverControllerA.cross().whileTrue(m_AlgaeArm.MoveAlgaeArm(true)); // When X is pressed it will make the arm go up
-    m_driverControllerA.circle().whileTrue(m_AlgaeArm.MoveAlgaeArm(false)); // When circle is pressed it will make the arm go down
+    m_driverControllerB.povUp().whileTrue(m_AlgaeArm.MoveAlgaeArm(false)); // When X is pressed it will make the arm go up
+    m_driverControllerB.povDown().whileTrue(m_AlgaeArm.MoveAlgaeArm(true)); // When circle is pressed it will make the arm go down
     
     // Algae controls:
     // Use L1, L2 for intake motor and R1, R2 for shooter motor
